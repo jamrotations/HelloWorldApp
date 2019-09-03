132fccb6049f88d0d76b39758865f4b20be379bd
dele <ayuze86@gmail.com>
2019-09-02 18:54:17 -0600

    changes 3

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorldApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Ojo!");
+            Console.WriteLine("Modify 3!");
         }
     }
 }
